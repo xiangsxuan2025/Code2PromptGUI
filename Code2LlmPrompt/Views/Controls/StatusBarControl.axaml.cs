f377ca79a68261593dd4cc238a1f8d5e95b0cbbc
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-11-27 14:44:22 +0800

    perf: UI调整：进度, rust程序可调用状态, 移到底部

---
+++ Code2LlmPrompt/Views/Controls/StatusBarControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Code2LlmPrompt.Views.Controls
+{
+    public partial class StatusBarControl : UserControl
+    {
+        public StatusBarControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
