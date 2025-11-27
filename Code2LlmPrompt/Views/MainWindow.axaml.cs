04b2686f30cf30f2893912b7eb034489c0451441
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-11-27 15:19:51 +0800

    styles: 自定义标题栏

--- Code2LlmPrompt/Views/MainWindow.axaml.cs
+++ Code2LlmPrompt/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Code2LlmPrompt.ViewModels;
 
 namespace Code2LlmPrompt.Views
@@ -14,6 +16,41 @@ namespace Code2LlmPrompt.Views
             {
                 viewModel.SetMainWindow(this);
             }
+        }
+
+        // 最小化按钮点击事件
+        private void MinimizeClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        // 最大化/还原按钮点击事件
+        private void MaximizeClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        // 关闭按钮点击事件
+        private void CloseClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // 允许通过标题栏拖动窗口
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                BeginMoveDrag(e);
+            }
+            base.OnPointerPressed(e);
         }
     }
 }
